81b4e405e3335a791ea4fbbce25dd90ac8824b56
Suryansh <104053578+Mahagola@users.noreply.github.com>
2025-12-26 12:35:58 +0530

    Add line sweep algorithm for meeting room calculation
    Implemented a line sweep algorithm to calculate the minimum number of meeting rooms required based on start and end times.

--- meeting-rooms-ii.cpp
+++ meeting-rooms-ii.cpp
@@ -22,3 +22,35 @@ int Solution::solve(vector<vector<int> > &A) {
     }
     return maxi;
 }   
+
+
+// using line sweep 
+class Solution {
+  public:
+    int minMeetingRooms(vector<int> &start, vector<int> &end) {
+        // code here
+        int n = start.size();
+        vector<pair<int, int>>v;
+        for(int i = 0;i<start.size();i++){
+            v.push_back({start[i], 1});
+            v.push_back({end[i], -1});
+        }
+        
+        sort(v.begin(), v.end(), [](const pair<int, int>&a, const pair<int, int>&b){
+            if(a.first!=b.first){
+                return a.first < b.first;
+            } 
+            return a.second < b.second;
+        });
+        
+        int maxi = 1;
+        int cnt = 0;
+        for(int i=0; i<v.size();i++){
+            cnt+=v[i].second;
+            if(cnt > 1){
+                maxi = max(cnt, maxi);
+            }
+        }
+        return maxi;
+    }
+};
